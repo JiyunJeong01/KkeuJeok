4410b7915b19d084c03110a6b2cc790eeb839adc
Jiyun Jeong <wjdwldbsskfk@gamil.com>
2024-06-25 15:41:27 +0900

    Controller제외 Memo 서비스 설정 node.js로 리팩토링

--- main.js
+++ main.js
@@ -1,18 +1,69 @@
-//main.js
 const express = require("express"), //애플리케이션에 express 모듈 추가
 layouts = require("express-ejs-layouts"), //모듈 설치
 methodOverride = require("method-override"),
-homeRouter = require('./routers/homeRouter');
+bodyParser = require('body-parser'),
+session = require('express-session');
 
-app = express(); //app에 express 웹 서버 애플리케이션 할당
-app.use(layouts); //레이아웃 사용
+app = express(); // app에 express 웹 서버 애플리케이션 할당
+require('dotenv').config();
+
+/////////////////////////////////////////
+// 세션 설정
+app.use(session({
+  secret: process.env.SECRET_KEY,
+  resave: false,
+  saveUninitialized: true
+}));
+
+app.use((req, res, next) => {
+  res.locals.user = req.session.user || null;
+  next();
+});
+
+// 테스트 : 세션 확인
+app.get('/session-data', (req, res) => {
+  if (req.session.user) {
+      res.json({
+          message: '세션 데이터가 있습니다.',
+          user: req.session.user
+      });
+  } else {
+      res.json({
+          message: '세션 데이터가 없습니다.'
+      });
+  }
+});
+/////////////////////////////////////////
+
+
+// JSON데이터의 최대 크기 설정
+app.use(bodyParser.json({ limit: '50mb' })); 
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
+
+
+// 레이아웃 설정
+app.use(layouts);
+app.use(express.static("public"));
+
+
+// 데이터 요청 및 파싱 처리
 app.use(methodOverride("_method", {methods: ["POST", "GET"]}));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
-app.set("port", process.env.PORT || 80); //포트 80으로 연결 셋팅
-app.set("view engine", "ejs"); //뷰 엔진을 ejs로 설정
 
+/////////////////////////////////////////
+// 라우트 설정
+const homeRouter = require('./routers/homeRouter');
 app.use("/", homeRouter);
+/////////////////////////////////////////
 
+// 포트 및 뷰 엔진 설정
+app.set("port", process.env.PORT || 80);
+app.set("view engine", "ejs");
+
+
+// 최종적으로 제대로 작동하는지 확인 
 app.listen(app.get("port"), () => {
   console.log(`Server running at http://localhost:${app.get("port")}`);
-}); //최종적으로 제대로 작동하는지 확인 
+});
