c15e728e2e943a23bdafe89bda49747271952189
Jiyun Jeong <wjdwldbsskfk@gamil.com>
2024-06-26 23:51:46 +0900

    변수명 변경 및 타임스탬프 데이터 처리

--- public/js/index.js
+++ public/js/index.js
@@ -1,11 +1,28 @@
+// timestamp를 형식에 맞게 변환
+function formatTimeStamp(timeStamp, dateSeparator = "-", timeSeparator = ":") {
+    var date = new Date(timeStamp* 1000); // 타임스탬프를 인자로 받아 Date 객체 생성
+
+    // 생성한 Date 객체에서 년, 월, 일, 시, 분, 초를 각각 문자열로 추출
+    var year   = date.getFullYear().toString();           // 년도
+    var month  = ("0" + (date.getMonth() + 1)).slice(-2); // 월 2자리 (01, 02 ... 12)
+    var day    = ("0" + date.getDate()).slice(-2);        // 일 2자리 (01, 02 ... 31)
+    var hour   = ("0" + date.getHours()).slice(-2);       // 시 2자리 (00, 01 ... 23)
+    var minute = ("0" + date.getMinutes()).slice(-2);     // 분 2자리 (00, 01 ... 59)
+    var second = ("0" + date.getSeconds()).slice(-2);     // 초 2자리 (00, 01 ... 59)
+
+    // 형식화된 문자열 생성
+    var formattedDateTime = `${year}${dateSeparator}${month}${dateSeparator}${day} ${hour}${timeSeparator}${minute}${timeSeparator}${second}`;
+
+    return formattedDateTime;
+}
 
 // 사용자가 내용을 올바르게 입력하였는지 확인합니다.
-function isValidContents(contents) {
-    if (contents == '') {
+function isValidcontent(content) {
+    if (content == '') {
         alert('내용을 입력해주세요');
         return false;
     }
-    if (contents.trim().length > 140) {
+    if (content.trim().length > 140) {
         alert('공백 포함 140자 이하로 입력해주세요');
         return false;
     }
@@ -16,8 +33,8 @@ function isValidContents(contents) {
 // 숨길 버튼을 숨기고, 나타낼 버튼을 나타냅니다.
 function editPost(id) {
     showEdits(id);
-    let contents = $(`#${id}-contents`).text().trim();
-    $(`#${id}-textarea`).val(contents);
+    let content = $(`#${id}-content`).text().trim();
+    $(`#${id}-textarea`).val(content);
 }
 
 function showEdits(id) {
@@ -25,22 +42,22 @@ function showEdits(id) {
     $(`#${id}-submit`).show();
     $(`#${id}-delete`).show();
 
-    $(`#${id}-contents`).hide();
+    $(`#${id}-content`).hide();
     $(`#${id}-edit`).hide();
 }
 
 // 메모를 생성합니다.
 function writePost() {
     // 1. 작성한 메모를 불러옵니다.
-    let contents = $('#contents').val();
+    let content = $('#content').val();
 
-    // 2. 작성한 메모가 올바른지 isValidContents 함수를 통해 확인합니다.
-    if (isValidContents(contents) == false) {
+    // 2. 작성한 메모가 올바른지 isValidcontent 함수를 통해 확인합니다.
+    if (isValidcontent(content) == false) {
         return;
     }
 
     // 3. 전달할 data JSON으로 만듭니다.
-    let data = { 'userId': 'test', 'content': contents };
+    let data = { 'userId': 'test', 'content': content };
 
     // 4. POST memo 에 data를 전달합니다.
     fetch('/memo', {
@@ -57,18 +74,18 @@ function writePost() {
 
 // 메모를 수정합니다.
 function submitEdit(memoId) {
-    // 1. 작성 대상 메모의 contents 를 확인합니다.
-    let contents = $(`#${memoId}-textarea`).val().trim();
+    // 1. 작성 대상 메모의 content 를 확인합니다.
+    let content = $(`#${memoId}-textarea`).val().trim();
 
-    // 2. 작성한 메모가 올바른지 isValidContents 함수를 통해 확인합니다.
-    if (isValidContents(contents) == false) {
+    // 2. 작성한 메모가 올바른지 isValidcontent 함수를 통해 확인합니다.
+    if (isValidcontent(content) == false) {
         return;
     }
 
     // 3. 전달할 data JSON으로 만듭니다.
-    let data = { 'content': contents };
+    let data = { 'content': content };
 
-    // 4. PUT /memos/{memoId} 에 data를 전달합니다.
+    // 4. PUT /memo/{memoId} 에 data를 전달합니다.
     fetch(`/memo/${memoId}`, {
         method: 'PUT',
         headers: {
